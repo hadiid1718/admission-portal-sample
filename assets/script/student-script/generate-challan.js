09b8a769de51b18e01eb6e83a95c3be1c93bd2ff
Hadeed Ul Hassan <hadeed.hassan189@gmail.com>
2025-11-04 11:02:39 +0500

    Ignore the assets folder

--- assets/script/student-script/generate-challan.js
+++
@@ -1,8 +0,0 @@
-        // Auto-focus on print button when page loads
-        window.onload = function() {
-            document.querySelector('.print-btn').focus();
-        }
-        
-        // Optional: Show print dialog automatically
-        // Uncomment the line below if you want auto-print
-        // window.onload = function() { window.print(); }
